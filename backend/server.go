d4175f45f9359437435d1d85bafda2126fa433fd
“artty1” <art_ty@hotmail.com>
2022-11-09 21:54:42 +0700

    call python file by go

--- backend/server.go
+++ backend/server.go
@@ -40,7 +40,8 @@ func main() {
 	e.GET("/RM/filter_bar/:area", d.GetFilterBarData)
 
 	c := cron.New()
-	c.AddFunc("5 * * * *", RunJob)
+	c.AddFunc("* * * * * *", RunJob)
+	// c.AddFunc("01 00 * * 1", RunJob)
 	go c.Start()
 
 	e.Logger.Fatal(e.Start(":1234"))
@@ -52,6 +53,22 @@ func RunJob() {
 	fmt.Println("cron job run ......")
 	fmt.Printf("%v\n", time.Now())
 
+	// RunJobByShellScript()
+	// RunJobByPython()
+	RunJobByPython()
+}
+
+func RunJobByPythonSayHello() {
+	cmd := exec.Command("python", "-c", "import hello;")
+	fmt.Println(cmd.Args)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(out))
+}
+
+func RunJobByShellScript() {
 	cmd := exec.Command("/bin/sh", "./First.sh")
 	cmd.Stdin = strings.NewReader("")
 	var out bytes.Buffer
@@ -61,5 +78,14 @@ func RunJob() {
 		log.Fatal(err)
 	}
 	fmt.Println("Output", out.String())
+}
 
+func RunJobByPython() {
+	cmd := exec.Command("python", "-c", "import pythonFile; pythonFile.cat_strings('foo', 'bar')")
+	fmt.Println(cmd.Args)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(out))
 }
